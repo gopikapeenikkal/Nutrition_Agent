a677e3a5fb3ce8327d5aa4a495b67058ab370520
GopikaPJ <pjgopika44@gmail.com>
2025-01-06 14:15:22 +0530

    update

--- freshi_nutrition.py
+++ freshi_nutrition.py
@@ -1,31 +1,40 @@
 import os
 from phi.agent import Agent
-from phi.knowledge.text import TextKnowledgeBase
+from phi.knowledge.json import JSONKnowledgeBase
 from phi.vectordb.pgvector import PgVector
 from phi.model.google import Gemini
-from dotenv import load_dotenv 
+from dotenv import load_dotenv
+# from products import all_products
+# from train import SYSTEM_PROMPT,INSTRUCTIONS
 
-load_dotenv 
+load_dotenv()
 
 GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
 
-knowledge = TextKnowledgeBase (
-    path = "https://uat.freshivores.com/v1/products",
-    vector_db = PgVector(
-        table_name = 'text_file',
-        db_url = 'postgresql+psycopg2://ai:freshi_ai@localhost:5532/freshi'   
-    ),  
-)
-web_Agent = Agent(
-    knowledge_base = knowledge,
-    model = Gemini(id="gemini-1.5-flash"),
-    search_knowledge = True,
-    
-) 
-web_Agent.print_response(
-    "get the ingredients for making biriyani from the provided text",
-    stream = True
-)
+# knowledge = JSONKnowledgeBase (
+#     path = "https://uat.freshivores.com/v1/products",
+#     vector_db = PgVector(
+#         table_name = 'freshi_json',
+#         db_url = 'postgresql+psycopg2://ai:freshi_ai@localhost:5432/freshi'   
+#     ),  
+# )
+
+# agent = Agent(
+#     knowledge_base = knowledge,
+#     model=Gemini(id="gemini-1.5-pro"),
+#     markdown=True,
+#     debug_mode=True,
+#     # search_knowledge = True,
+#     system_prompt="you are an agent who provides information about the products in the knowledge, if any other questions asked then say you do not have any information about it",
+#     instruction="take the information of that product from knowledge_base these all are the products provided by the brand freshivores, and give a brif idea about the product",
+#     structured_outputs=True,
+# ) 
+# agent.knowledge.load(recreate=False) 
+# agent.print_response("jackfruit dried")
+# web_Agent.print_response(
+#     "jack fruit dried",
+#     stream = True
+# )
 
 # db_url = 'postgresql+psycopg://ai:freshi_ai@localhost:5532/freshi'
 
@@ -34,6 +43,15 @@ web_Agent.print_response(
 ## product page --> 
 ## take the product title --> 
 ## collect the product details from the api --> 
-# pass that to LLM --> 
+## pass that to LLM --> 
+#taking input to amke the diet plan
+
 # Instruction to take the text and modify it with *a comparision to the other packled products commomly avilable *highlight nutrition benefit of this product in short --> 
 # structured response 
+
+agent = Agent(
+    model=Gemini(id="gemini-1.5-flash"),
+    markdown=True,
+)
+
+agent.print_response("Share a 2 sentence horror story.")
--- products.py
+++ products.py
@@ -35,7 +35,44 @@ def get_product_detail(product_id:int):
     except Exception as e:
         print(f"Exception error in --> get_product_detail{e}") 
 
-get_product_detail()
+# get_product_detail()
+
+#to get all product data
+
+
+def all_products():
+    #loads json
+    products_json = requests.get ("https://uat.freshivores.com/v1/products")
+    
+    #extract json data
+    products = products_json.json()
+    for product in products:
+        title = product['title'] or '',
+        short_description = product['short_description'] or '',
+        description = product['description'] or '',
+        additional_info = product['additional_info'] or ''
+        product_data = {"Title":title,
+                        "Short_description":short_description,
+                        "Description":description,
+                        "Additional_info":additional_info
+        }
+        print(product_data)
+        return product_data 
+# all_products() 
+
+
+#spliting the inputs for the nutrition diet 
+# a,b,c,d = input("Enter the following details Age,Gender,Height,Weight:").split() 
+# A = "Age:",a
+# B = "Gender:",b
+# C = "Height:",c
+# D = "Weight:",d
+# print(A)
+# print(B)
+# print(C)
+# print(D)
+# print()
+
 
 
     
